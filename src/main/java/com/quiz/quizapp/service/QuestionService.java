be01c1a832efdb7e09084075969a0bdd9c3115c8
pamudu <pamuduprabathiya28@gmail.com>
2023-07-06 10:41:32 +0530

    add QuestionService class

---
+++ src/main/java/com/quiz/quizapp/service/QuestionService.java
@@ -0,0 +1,18 @@
+package com.quiz.quizapp.service;
+
+import com.quiz.quizapp.dao.QuestionDAO;
+import com.quiz.quizapp.entity.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDAO questionDAO;
+    public List<Question> getAllQuestions() {
+        return questionDAO.findAll();
+    }
+}
