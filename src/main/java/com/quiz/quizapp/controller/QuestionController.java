cdd0fd10c9485625bbbd6632508e453a33d5879a
pamudu <pamuduprabathiya28@gmail.com>
2023-07-06 10:42:23 +0530

    refactor question controller class

--- src/main/java/com/quiz/quizapp/controller/QuestionController.java
+++ src/main/java/com/quiz/quizapp/controller/QuestionController.java
@@ -1,15 +1,22 @@
 package com.quiz.quizapp.controller;
 
+import com.quiz.quizapp.entity.Question;
+import com.quiz.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
 
+    @Autowired
+    QuestionService questionService;
     @GetMapping("/allQuestions")
-    public String allQuestions(){
-        return "These are the questions .....";
+    public List<Question> allQuestions(){
+        return questionService.getAllQuestions();
     }
 }
