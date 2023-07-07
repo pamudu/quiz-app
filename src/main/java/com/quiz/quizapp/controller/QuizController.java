fba07371cc04c39dddc198bd0dedb6bc9802ece9
pamudu <pamuduprabathiya28@gmail.com>
2023-07-07 11:56:31 +0530

    add the get quiz feature

--- src/main/java/com/quiz/quizapp/controller/QuizController.java
+++ src/main/java/com/quiz/quizapp/controller/QuizController.java
@@ -1,9 +1,13 @@
 package com.quiz.quizapp.controller;
 
+import com.quiz.quizapp.entity.Question;
+import com.quiz.quizapp.model.QuestionWrapper;
 import com.quiz.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -17,5 +21,9 @@ public class QuizController {
                                              @RequestParam String title){
         System.out.println("Hello");
         return quizService.createQuiz(category, numQ, title);
+    }
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuiz(@PathVariable int id){
+        return quizService.getQuizById(id);
     }
 }
--- src/main/java/com/quiz/quizapp/service/QuizService.java
+++ src/main/java/com/quiz/quizapp/service/QuizService.java
@@ -4,12 +4,15 @@ import com.quiz.quizapp.dao.QuestionDAO;
 import com.quiz.quizapp.dao.QuizDAO;
 import com.quiz.quizapp.entity.Question;
 import com.quiz.quizapp.entity.Quiz;
+import com.quiz.quizapp.model.QuestionWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -28,5 +31,15 @@ public class QuizService {
         quizDAO.save(quiz);
 
         return new ResponseEntity<>("Quiz saved", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizById(int id) {
+        Optional<Quiz> quiz = quizDAO.findById(id);
+        List<Question> questionList = quiz.get().getQuestions();
+        List<QuestionWrapper> qw = new LinkedList<>();
+        for(Question q : questionList){
+            qw.add(new QuestionWrapper(q.getId(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4(),q.getQuestionTitle()));
+        }
+        return new ResponseEntity<>(qw,HttpStatus.OK);
     }
 }
